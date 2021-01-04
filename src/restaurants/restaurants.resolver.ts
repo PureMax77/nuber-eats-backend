d789c4174b27c724bc27168a59e73fc9c7d06dba
PureMax77 <ertr777@naver.com>
2021-01-05 01:25:40 +0900

    1.5까지 수강

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    console.log(createRestaurantInput);
+    return true;
+  }
+}
